efdf1c0ac2b4b5a9f2dcd82ac9fc3c3f370e10b2
maximdolphin <maximka.dolphin@gmail.com>
2025-03-01 15:54:27 -0500

    Add a new degree service for fetching and add variables to fetch service

--- src/api/api_fetch/services.py
+++ src/api/api_fetch/services.py
@@ -1,6 +1,9 @@
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
+from src.api.api_fetch.models import Requirement
+
+
 class PandaService:
     def __init__(self, session_cookie: str):
         self.session_cookie = session_cookie
@@ -10,9 +13,9 @@ class PandaService:
         )
         self.client = Client(transport=transport, fetch_schema_from_transport=True)
 
-    def fetch_panda(self, query: str) -> dict:
+    def fetch_panda(self, query: str, variables: dict[str, any] | None) -> dict:
         query_obj = gql(query)
-        return self.client.execute(query_obj)
+        return self.client.execute(query_obj, variable_values=variables)
 
 class UserService:
     def __init__(self, panda_service: PandaService):
@@ -45,4 +48,22 @@ class UserService:
             }
           }
         """
-        return self.panda.fetch_panda(query)
+        return self.panda.fetch_panda(query, None)
+
+class DegreeService:
+    def __init__(self, panda_service: PandaService):
+        self.panda = panda_service
+
+    def get_degree_req(self, degree_name: str):
+        query = """
+        query GetRequirements($degreeName: String) {
+          getRequirements(degreeName: $degreeName) {
+            id
+            category
+            reqType
+            classIds
+            degreeId
+          }
+        }
+        """
+        return self.panda.fetch_panda(query, {"degreeName": degree_name})
