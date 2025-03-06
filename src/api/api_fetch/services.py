e2e2c81fe88fe3c847a40d34f45ad348cf307430
maximdolphin <maximka.dolphin@gmail.com>
2025-03-06 00:46:13 -0500

    Adjusted the services to transform JSON to their according models

--- src/api/api_fetch/services.py
+++ src/api/api_fetch/services.py
@@ -1,7 +1,9 @@
+from typing import Any, List
+
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
-from src.api.api_fetch.models import Requirement
+from src.api.api_fetch.models import UserModel, RequirementModel
 
 
 class PandaService:
@@ -13,7 +15,7 @@ class PandaService:
         )
         self.client = Client(transport=transport, fetch_schema_from_transport=True)
 
-    def fetch_panda(self, query: str, variables: dict[str, any] | None) -> dict:
+    def fetch_panda(self, query: str, variables: dict[str, any] | None) -> dict[str, Any]:
         query_obj = gql(query)
         return self.client.execute(query_obj, variable_values=variables)
 
@@ -25,13 +27,32 @@ class UserService:
         query = """
           query GetUser {
             getUser {
-              id
               email
               university
               isPremium
               yearInUniversity
               graduationSemesterName
               gpa
+              tasks {
+                id
+                title
+                description
+                dueDate
+                stageId
+                classCode
+                source
+              }
+              classSchedules {
+                id
+                title
+                isCurrent
+                semesterId
+              }
+              degreePlanners {
+                id
+                title
+                degreeId
+              }
               attendancePercentage
               assignmentCompletionPercentage
               takenClassIds
@@ -48,7 +69,10 @@ class UserService:
             }
           }
         """
-        return self.panda.fetch_panda(query, None)
+
+        output = self.panda.fetch_panda(query, None)
+        user_model = UserModel.model_validate(output["getUser"])
+        return user_model
 
 class DegreeService:
     def __init__(self, panda_service: PandaService):
@@ -66,4 +90,6 @@ class DegreeService:
           }
         }
         """
-        return self.panda.fetch_panda(query, {"degreeName": degree_name})
+        output = self.panda.fetch_panda(query, {"degreeName": degree_name})
+        requirement_model: List[RequirementModel] = [RequirementModel.model_validate(requirement) for requirement in output["getRequirements"]]
+        return requirement_model
