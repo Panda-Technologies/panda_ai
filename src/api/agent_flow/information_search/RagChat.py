95a9ae18a5b412bbcaf05b73c5acf0e2452f14e0
maximdolphin <maximka.dolphin@gmail.com>
2025-03-06 00:47:49 -0500

    Significantly adjusted RagChat to take in more parameters and become more coupled

--- src/api/agent_flow/information_search/RagChat.py
+++ src/api/agent_flow/information_search/RagChat.py
@@ -3,16 +3,14 @@ import logging
 from pydantic import SecretStr
 
 from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai import FunctionChoiceBehavior, PromptExecutionSettings
 from semantic_kernel.connectors.ai.open_ai import (
     AzureAISearchDataSource,
-    AzureChatCompletion,
     AzureChatPromptExecutionSettings,
     ExtraBody,
 )
 from semantic_kernel.connectors.memory.azure_cognitive_search.azure_ai_search_settings import AzureAISearchSettings
-from semantic_kernel.contents import ChatHistory, ChatMessageContent
 from semantic_kernel.functions import KernelArguments, FunctionResult
-from semantic_kernel.processes.kernel_process import KernelProcessStep, KernelProcessStepState
 from semantic_kernel.prompt_template import InputVariable, PromptTemplateConfig
 from dotenv import load_dotenv
 import os
@@ -20,11 +18,11 @@ import os
 from src.api.agent_flow.chat_flow.ConversationContext import ConversationContext
 
 
-class AzureRagChat(KernelProcessStep[ConversationContext]):
-    kernel: Kernel | None = None
+class AzureRagChat:
 
-    def __init__(self, load_env_vars=True):
-        super().__init__()
+    def __init__(self, state: ConversationContext, kernel: Kernel, prompt_template: PromptTemplateConfig, query: str, load_env_vars=True):
+        self.state = state
+        self.kernel = kernel
         if load_env_vars:
             load_dotenv()
 
@@ -35,6 +33,9 @@ class AzureRagChat(KernelProcessStep[ConversationContext]):
             api_key=SecretStr(os.environ.get("AZURE_AISEARCH_KEY")),
         )
 
+        self.prompt_template = prompt_template
+        self.query = query
+
         # Set up RAG data source
         self.az_source = AzureAISearchDataSource.from_azure_ai_search_settings(
             azure_ai_search_settings=self.azure_ai_search_settings
@@ -42,31 +43,18 @@ class AzureRagChat(KernelProcessStep[ConversationContext]):
         self.extra_body = ExtraBody(data_sources=[self.az_source])
         self.req_settings = AzureChatPromptExecutionSettings(service_id="default", extra_body=self.extra_body)
 
-        # Set up the chat function
         self._setup_chat_function()
-
-    def activate(self, state: KernelProcessStepState[ConversationContext]):
-        self.state = state or ConversationContext()
 
     def _setup_chat_function(self):
         """Set up the chat function with the correct prompt template."""
-        self.prompt_template_config = PromptTemplateConfig(
-            template="{{$chat_history}}\nUser: {{$user_input}}\nAssistant:",
-            name="chat",
-            template_format="semantic-kernel",
-            input_variables=[
-                InputVariable(name="chat_history", description="The chat history", is_required=True),
-                InputVariable(name="user_input", description="The user input", is_required=True),
-            ],
-            execution_settings={"default": self.req_settings},
-        )
+        self.prompt_template_config = self.prompt_template
+        self.prompt_template_config.add_execution_settings(self.req_settings)
 
-        if self.kernel:
-            self.chat_function = self.kernel.add_function(
-                plugin_name="ChatBot",
-                function_name="Chat",
-                prompt_template_config=self.prompt_template_config
-            )
+        self.chat_function = self.kernel.add_function(
+            plugin_name="ChatBot",
+            function_name="Chat",
+            prompt_template_config=self.prompt_template_config,
+        )
 
     async def generate_response(
             self,
@@ -84,17 +72,6 @@ class AzureRagChat(KernelProcessStep[ConversationContext]):
             Tuple of (response text, updated chat history)
         """
 
-        # Prepare arguments for kernel invocation
-        arguments = KernelArguments(
-            chat_history=self.state.to_chat_history(),
-            user_input=user_input,
-            execution_settings=self.req_settings
-        )
-
         # Generate response (non-streaming for now)
-        response = await self.kernel.invoke(self.chat_function, arguments=arguments)
-
-        self.state.add_user_message(user_input)
-        self.state.add_assistant_message(str(response))
-
+        response = await self.kernel.invoke(self.chat_function, arguments=KernelArguments(query=self.query))
         return response
